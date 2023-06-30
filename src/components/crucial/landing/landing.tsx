7d711e17e3338c27d97b3e9cdaba0ba213222b2a
Hari Vengadesh <ehvenga@gmail.com>
2023-07-01 02:23:28 +0530

    api calls in qwik

--- src/components/crucial/landing/landing.tsx
+++ src/components/crucial/landing/landing.tsx
@@ -1,11 +1,17 @@
 import { component$, useStylesScoped$ } from '@builder.io/qwik';
 import styles from './landing.css?inline';
 
+import PokemonCard from './pokemonCard/pokemonCard';
+
 export default component$(() => {
   useStylesScoped$(styles);
   return (
     <main>
       <h3>Choose your favourite shiny</h3>
+      <section>
+        <PokemonCard />
+        <PokemonCard />
+      </section>
     </main>
   );
 });
